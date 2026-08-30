920c3f6f1dca71b421d4e157efe97c4026779a08
kalilrodrigues827-crypto <kalilrodrigues827@gmail.com>
2026-08-30 05:30:49 -0300

    chore: bump Jarvis to 2.0.0 alpha01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rushworks.jarvis"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+versionName = "2.0.0-alpha01"
     }
 
     compileOptions {
